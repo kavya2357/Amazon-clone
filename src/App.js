e70ca44b03fc318e2f78790f65176ddf30156502
kavya2357 <nilipalli.kavya2020@vitstudent.ac.in>
2022-09-16 23:22:41 +0530

    Checkout page created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
 import Header from './Header.js'
 import Home from './Home.js'
+import Checkout from './Checkout.js'
+import {BrowserRouter, Routes,Route} from "react-router-dom";
 function App() {
   return (
-    <div className="app">
-      {/* header*/}
-      <Header />
-      {/*home */}
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <Routes>
+          {/* <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+
+
+        {/* <Header />
+        <Home /> */}
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/Checkout.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Checkout.css"
+function Checkout() {
+  return (
+    <div className='checkout'>
+        <div className='left'>
+          <div className='title'>
+            <h1>Shopping cart</h1>
+            <p>Deselect all items</p>
+          </div>
+          <div>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className='right'>
+            <div className='top'>
+
+            </div>
+
+            <div className='bottom'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Checkout
